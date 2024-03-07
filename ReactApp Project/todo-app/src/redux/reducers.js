8f41beb17990fec113f446a668d668aff52075ce
chirag-chawla <cchawla890@gmail.com>
2024-03-07 21:31:16 +0530

    Added the 4 properties to todo and modified the UI. Second draft

--- ReactApp Project/todo-app/src/redux/reducers.js
+++ ReactApp Project/todo-app/src/redux/reducers.js
@@ -1,37 +1,40 @@
 // src/redux/reducers.js
 const initialState = {
-    todos: [],
-  };
-  
-  const todoReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'ADD_TODO':
-        return {
-          ...state,
-          todos: [
-            ...state.todos,
-            {
-              id: state.todos.length + 1,
-              text: action.payload.text,
-              completed: false,
-            },
-          ],
-        };
-  
-      case 'TOGGLE_TODO':
-        return {
-          ...state,
-          todos: state.todos.map((todo) =>
-            todo.id === action.payload.id
-              ? { ...todo, completed: !todo.completed }
-              : todo
-          ),
-        };
-  
-      default:
-        return state;
-    }
-  };
-  
-  export default todoReducer;
-  
+  todos: [],
+};
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      const newTodo = {
+        id: state.todos.length + 1,
+        title: action.payload.title,
+        description: action.payload.description,
+        dueDate: action.payload.dueDate ? new Date(action.payload.dueDate) : '',
+        priority: action.payload.priority,
+        completed: false,
+      };
+
+      return {
+        ...state,
+        todos: [...state.todos, newTodo].sort((a, b) =>
+          a.priority === 'high' ? -1 : 1
+        ),
+      };
+
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.id
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
